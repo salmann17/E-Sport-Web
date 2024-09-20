fb46e83144d1f0e1ebc6078e57b40feddb024d90
Salman Alfarizi <salmanlfaa11@gmail.com>
2024-09-20 08:47:25 +0700

    update

--- backend/event/editevent_proses.php
+++ backend/event/editevent_proses.php
@@ -1,47 +1,47 @@
 <?php
-$idevent = $_POST['idevent']; 
-$name = $_POST['name']; 
-$desc = $_POST['desc']; 
-$date = $_POST['date'];
-$new_teams = [];
-if(isset($_POST['team'])){
-    $new_teams = $_POST['team'];
-}
-
-$mysqli = new mysqli("localhost", "root", "", "esport");
-    if($mysqli -> connect_errno){
-        echo "failed to connect to mysql;" . $mysqli-> connect_error;
+    $idevent = $_POST['idevent']; 
+    $name = $_POST['name']; 
+    $desc = $_POST['desc']; 
+    $date = $_POST['date'];
+    $new_teams = [];
+    if(isset($_POST['team'])){
+        $new_teams = $_POST['team'];
     }
 
-$update_event = $mysqli->prepare("UPDATE event SET name = ?, description = ?, date = ? WHERE idevent = ?");
-$update_event->bind_param("sssi", $name, $desc, $date, $idevent);
-$update_event->execute();
+    $mysqli = new mysqli("localhost", "root", "", "esport");
+        if($mysqli -> connect_errno){
+            echo "failed to connect to mysql;" . $mysqli-> connect_error;
+        }
 
-$current_teams = [];
-$query_teams = $mysqli->prepare("SELECT idteam FROM event_teams WHERE idevent = ?");
-$query_teams->bind_param("i", $idevent);
-$query_teams->execute();
-$result = $query_teams->get_result();
-while ($row = $result->fetch_assoc()) {
-    $current_teams[] = $row['idteam'];
-}
+    $update_event = $mysqli->prepare("UPDATE event SET name = ?, description = ?, date = ? WHERE idevent = ?");
+    $update_event->bind_param("sssi", $name, $desc, $date, $idevent);
+    $update_event->execute();
 
-foreach ($new_teams as $idteam) {
-    if (!in_array($idteam, $current_teams)) {
-        $insert_team = $mysqli->prepare("INSERT INTO event_teams (idevent, idteam) VALUES (?, ?)");
-        $insert_team->bind_param("ii", $idevent, $idteam);
-        $insert_team->execute();
+    $current_teams = [];
+    $query_teams = $mysqli->prepare("SELECT idteam FROM event_teams WHERE idevent = ?");
+    $query_teams->bind_param("i", $idevent);
+    $query_teams->execute();
+    $result = $query_teams->get_result();
+    while ($row = $result->fetch_assoc()) {
+        $current_teams[] = $row['idteam'];
     }
-}
 
-foreach ($current_teams as $idteam) {
-    if (!in_array($idteam, $new_teams)) {
-        $delete_team = $mysqli->prepare("DELETE FROM event_teams WHERE idevent = ? AND idteam = ?");
-        $delete_team->bind_param("ii", $idevent, $idteam);
-        $delete_team->execute();
+    foreach ($new_teams as $idteam) {
+        if (!in_array($idteam, $current_teams)) {
+            $insert_team = $mysqli->prepare("INSERT INTO event_teams (idevent, idteam) VALUES (?, ?)");
+            $insert_team->bind_param("ii", $idevent, $idteam);
+            $insert_team->execute();
+        }
     }
-}
-$mysqli->close();
-header("Location: ../dbevent.php");
-exit();
+
+    foreach ($current_teams as $idteam) {
+        if (!in_array($idteam, $new_teams)) {
+            $delete_team = $mysqli->prepare("DELETE FROM event_teams WHERE idevent = ? AND idteam = ?");
+            $delete_team->bind_param("ii", $idevent, $idteam);
+            $delete_team->execute();
+        }
+    }
+    $mysqli->close();
+    header("Location: ../dbevent.php");
+    exit();
 ?>
