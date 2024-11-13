4cbe64d41be87c2b0857c95b912fa59a847da943
Ferdi <ferdinandkusumasanjaya@gmail.com>
2024-11-13 22:36:17 +0700

    Hapus Gambar FIX - Ferdi

--- backendAdmin/team/delete_image.php
+++ backendAdmin/team/delete_image.php
@@ -2,15 +2,15 @@
 require_once("../models/team.php");
 session_start();
 
-    if (!isset($_SESSION['userid'])) {
-        $domain = $_SERVER['HTTP_HOST'];
-        $path = $_SERVER['SCRIPT_NAME'];
-        $queryString = $_SERVER['QUERY_STRING'];
-        $url = "http://" . $domain . $path . "?" . $queryString;
-    
-        header("location: ..\..\backendMember\member\dblogin.php?url_asal=".$url);
-        exit();
-    }
+if (!isset($_SESSION['userid'])) {
+    $domain = $_SERVER['HTTP_HOST'];
+    $path = $_SERVER['SCRIPT_NAME'];
+    $queryString = $_SERVER['QUERY_STRING'];
+    $url = "http://" . $domain . $path . "?" . $queryString;
+
+    header("location: ..\..\backendMember\member\dblogin.php?url_asal=" . $url);
+    exit();
+}
 
 if (isset($_GET['idteam'])) {
     $idteam = $_GET['idteam'];
@@ -24,4 +24,6 @@ if (isset($_GET['idteam'])) {
     header("Location: editteam.php?idteam=" . $idteam);
     exit();
 }
+header("Location: ../dbteam.php");
+exit();
 ?>
